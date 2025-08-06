2165d2844b8ecc2636c0ed6eee6fa119768ceb5d
Lovertab <lovertabrown@gmail.com>
2025-08-06 16:07:05 -0400

    updated the list of volleyball teams to add it to one list

--- Question2_athlete_heights/src/webscraping.py
+++ Question2_athlete_heights/src/webscraping.py
@@ -2,25 +2,37 @@
 import requests
 from bs4 import BeautifulSoup
 
-#Setting the URLs for the men's volleyball teams 
+#Setting the URLs for the teams 
+urls_list=[
+    'https://bmccathletics.com/sports/womens-volleyball/roster?view=2','https://yorkathletics.com/sports/womens-volleyball/roster',
+    'https://hostosathletics.com/sports/womens-volleyball/roster?view=2','https://bronxbroncos.com/sports/womens-volleyball/roster/2021?view=2',
+    'https://queensknights.com/sports/womens-volleyball/roster?view=2','https://augustajags.com/sports/wvball/roster',
+    'https://flaglerathletics.com/sports/womens-volleyball/roster?view=2','https://pacersports.com/sports/womens-volleyball/roster',
+    'https://www.lockhavenathletics.com/sports/womens-volleyball/roster',
+    'https://ccnyathletics.com/sports/mens-volleyball/roster?view=2','https://lehmanathletics.com/sports/mens-volleyball/roster?view=2','https://www.brooklyncollegeathletics.com/sports/mens-volleyball/roster?view=2',
+    'https://johnjayathletics.com/sports/mens-volleyball/roster?view=2','https://athletics.baruch.cuny.edu/sports/mens-volleyball/roster?view=2','https://mecathletics.com/sports/mens-volleyball/roster?view=2','https://www.huntercollegeathletics.com/sports/mens-volleyball/roster','https://yorkathletics.com/sports/mens-volleyball/roster',
+    'https://ballstatesports.com/sports/mens-volleyball/roster'
+    
+]
 
-city_college_of_ny='https://ccnyathletics.com/sports/mens-volleyball/roster?view=2'
-lehman_college='https://lehmanathletics.com/sports/mens-volleyball/roster?view=2'
-brooklyn_college='https://www.brooklyncollegeathletics.com/sports/mens-volleyball/roster?view=2'
-john_jay_college='https://johnjayathletics.com/sports/mens-volleyball/roster?view=2'
-baruch_college='https://athletics.baruch.cuny.edu/sports/mens-volleyball/roster?view=2'
-medgar_evers_college='https://mecathletics.com/sports/mens-volleyball/roster?view=2'
-hunter_college='https://www.huntercollegeathletics.com/sports/mens-volleyball/roster'
-york_college='https://yorkathletics.com/sports/mens-volleyball/roster'
-ball_state='https://ballstatesports.com/sports/mens-volleyball/roster'
 
-#Setting the URLs for the women's volleyball teams 
-bmcc='https://bmccathletics.com/sports/womens-volleyball/roster?view=2'
-york_college_women='https://yorkathletics.com/sports/womens-volleyball/roster'
-hostos_cc='https://hostosathletics.com/sports/womens-volleyball/roster?view=2'
-bronx_cc='https://bronxbroncos.com/sports/womens-volleyball/roster/2021?view=2'
-queens_college='https://queensknights.com/sports/womens-volleyball/roster?view=2'
-augusta_college='https://augustajags.com/sports/wvball/roster'
-flagler_college='https://flaglerathletics.com/sports/womens-volleyball/roster?view=2'
-usc_aiken='https://pacersports.com/sports/womens-volleyball/roster'
-penn_state_lock_haven='https://www.lockhavenathletics.com/sports/womens-volleyball/roster'
+
+
+# Set the headers and make a request to the server
+headers = {
+  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+  'Accept-Language': 'en-US,en;q=0.9',
+  'Connection': 'keep-alive'
+  }
+
+# make a request to the server
+page= requests.get(urls_list, headers=headers, verify=False)
+
+print(page.status_code)
+
+# look at raw html
+print(page.content)
+# import raw html into beautifulsoup
+soup=BeautifulSoup(page.content,'html.parser')
+print(soup.prettify())
